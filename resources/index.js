ca177a959e3e5da1716322a7a00ac249fe3cf817
mesimms1 <mesimms1@yahoo.com>
2021-02-03 19:18:22 -0800

    re-did html form. created javascript validation

---
+++ resources/index.js
@@ -0,0 +1,76 @@
+/*const newTaskNameInput = document.querySelector('#newTaskNameInput');
+const name = newTaskNameInput.value;*/
+
+const form = document.querySelector('#taskForm');
+
+const taskName = document.querySelector('#taskName');
+const taskNameValue = taskName.value.trim();
+
+const taskDescription = document.querySelector('#taskDescription');
+const taskDescriptionValue = taskDescription.value.trim();
+
+const assignTo = document.querySelector('#assignTo');
+const assignToValue = assignTo.value.trim();
+
+const datePicker = document.querySelector('#datepicker');
+const datePickerValue = datePicker.value.trim();
+
+const submitButton = document.querySelector('#submit-button');
+const submitButtonValue = submitButton.value.trim();
+
+const alert = document.querySelector('#alert').hidden = true;
+
+form.addEventListener('submit', = 
+
+function formValidation() {
+    let errorMessage = "";
+
+    if (taskNameValue === "") {
+        errorMessage +=("Name must be filled out. \n");
+        document.getElementById('formError').innerHTML = 'First Name not filled out. You silly goose.';
+        }
+
+    if (taskDescriptionValue === "") {
+        errorMessage +=("Description must be filled out. \n");
+        document.getElementById('formError').innerHTML = 'The Description was not filled out. You silly goose.';
+    }
+
+    if (assignToValue === "" || assignToValue === "AssignaCatSitter") {
+        errorMessage +=("Please assign someone this task \n");
+        document.getElementById('formError').innerHTML = 'Please assign this task to someone. You silly goose.';
+    }
+
+    if (datePickerValue === "") {
+        errorMessage +=("Due Date must be filled out. \n");
+        document.getElementById('formError').innerHTML = 'Tells us when you would like this done by. You silly goose.';
+    }
+
+    if (errorMessage !== "") {
+        alert(errorMessage);
+        return false;
+    }
+//Found this Youtube video helpful: https://www.youtube.com/watch?v=Pc2e2YpKArg
+//And this followup video for multiple fields: https://www.youtube.com/watch?v=xvXtb7mwMd8 
+
+/*
+validFormFieldInput = (data) =>{
+    if (taskNameValue === "") {
+        alert("Name must be filled out");
+        return false;
+      } else if (taskDescriptionValue === "") {
+          alert("Description must be filled out");
+          return false;
+      } else if (assignToValue === "") {
+          alert("Assign To Field must be filled out");
+          return false;
+      } else if (datePickerValue === "") {
+          alert("Due by date must be filled out");
+          return false;
+      } else {
+        console.log("Name:  "+ taskNameValue);
+        console.log("Description: " + taskDescriptionValue);
+        console.log("Assigned To: " + assignToValue);
+        console.log("Due Date: " + datePickerValue);
+      }
+}
+*/
