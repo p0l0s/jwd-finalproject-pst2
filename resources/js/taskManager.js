c0cd2de269f734c91afb05c61ce1379d7c13612d
mesimms1 <mesimms1@yahoo.com>
2021-02-05 08:46:03 -0800

    debugged taskmanager.js

--- resources/js/taskManager.js
+++ resources/js/taskManager.js
@@ -1,25 +1,21 @@
 class TaskManager {
-    constructor(currentId = 0)
-    {
+    constructor(currentId = 0){
         this.tasks = [];
         this.currentId = currentId;
     }
 
-    addTask (name, description, person, datepicker, status = 'TODO')
-    {
+    addTask (name, description, person, datepicker, status = 'TODO') {
         this.tasks.push({
-            'id':this.currentId
+            'id':this.currentId,
             name,
             description,
             'assignedTo': person,
             'dueDate': datepicker,
             status,
-            
-        });
-        this.currentId ++ 1;
+            });
+        this.currentId ++;
     }
-
-    }
+}
    
 //const test = new TaskManager();
 //console.log(test.task);
