859f6d986a4089c5c887f86aa1c913f2cea82058
mesimms1 <mesimms1@yahoo.com>
2021-02-04 17:33:44 -0800

    set currentId default to 0

--- resources/js/taskManager.js
+++ resources/js/taskManager.js
@@ -1,7 +1,6 @@
 class TaskManager {
-    constructor(currentId) {
+    constructor(currentId = 0) {
         this.task = ['item 1', 'item 2'];
-        currentId = 0;
         this.currentId = currentId;
     }
    addTask(name1, description, assign, datePicker, status) {
