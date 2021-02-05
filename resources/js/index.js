01ce5a7c93e229614e8e757097cc0c0ec6511a98
mesimms1 <mesimms1@yahoo.com>
2021-02-04 17:23:22 -0800

    made sure I had all of Tanya's solution. And moved commented out functions to bottom

--- resources/js/index.js
+++ resources/js/index.js
@@ -36,47 +36,7 @@ const currentDate = new Date;
 const todayDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
 const convertedTodayDate = Date.parse(todayDate);
 
-/*
-function formValidation() {
-    let errorMessage = "";
 
-    if (taskNameValue === "") {
-        errorMessage += "Name must be filled out. \n";
-        /*document.getElementById('formError').innerHTML = 'Name not filled out. You silly goose.'; */
-/*        }
-/*
-    if (taskDescriptionValue === "") {
-        errorMessage += "Description must be filled out. \n";
-        /*document.getElementById('formError').innerHTML = 'The Description was not filled out. You silly goose.'; */
- /*   }
-
-    if (assignToValue === "") {
-        errorMessage += "Please assign someone this task. \n";
-        /*document.getElementById('formError').innerHTML = 'Please assign this task to someone. You silly goose.'; */
-/*    }
-
-    /*if (datePickerValue === "") {
-        errorMessage +=("Due Date must be filled out. \n");
-        document.getElementById('formError').innerHTML = 'Tells us when you would like this done by. You silly goose.';
-    } */
-
- /*   if (errorMessage !== "") {
-        //alert(errorMessage);
-        document.getElementById('formError').innerHTML = errorMessage + "You silly goose.";
-        return false;
-    }
-    /*else {
-        console.log("Name:  " + taskNameValue);
-        console.log("Description:  " + taskDescriptionValue);
-        console.log("Assigned To:  " + assignToValue);
-        console.log("Due By:  " + datePickerValue);
-    }*/
-/*}
-*/ 
-
-
-//Found this Youtube video helpful: https://www.youtube.com/watch?v=Pc2e2YpKArg
-//And this followup video for multiple fields: https://www.youtube.com/watch?v=xvXtb7mwMd8 
 //Ended up Using this method: https://www.youtube.com/watch?v=In0nB0ABaUk&t=271s 
 
 //Log Values into console after hitting submit button. Listener in html
@@ -131,8 +91,119 @@ taskForm.addEventListener('submit', (e) => {
         }
 })
 
+const test = new TaskManager();
+console.log(test.task);
 
 
+
+
+
+
+/*
+//Tanya's solution to validation challenge:
+
+let saveFile = () => {
+    	
+    // Get the data from each element on the form.
+    const name = document.getElementById('name');
+    const description = document.getElementById('description');
+    const person = document.getElementById('person');
+    const datepicker = document.getElementById('datepicker');
+    const convertedDatepicker = Date.parse(datepicker.value);
+    const currentDate = new Date;
+    const todayDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+    const convertedTodayDate = Date.parse(todayDate);
+
+    
+    if(!name.value||!description.value||!person.value||!datepicker.value)
+    {
+        document.getElementById("alertm").className = "alert alert-danger";
+       
+    document.getElementById("alertm").innerHTML="<strong>Error!</strong> Please fill all fields!";
+}
+    else if (name.value.trim() == ""||description.value.trim() == ""||person.value.trim() == "")
+    {
+        document.getElementById("alertm").className = "alert alert-danger";
+        document.getElementById("alertm").innerHTML="<strong>Error!</strong> Please don't use only whitespaces!";}
+        
+    else if (convertedDatepicker<convertedTodayDate)
+    {console.log(convertedDatepicker);
+        console.log(convertedTodayDate);
+        document.getElementById("alertm").className = "alert alert-danger";
+        document.getElementById("alertm").innerHTML="<strong>Error!</strong> Please assign correct date!";
+        }
+   else { 
+    document.getElementById("alertm").className = "";
+    document.getElementById("alertm").innerHTML="";
+    
+    
+    // This variable stores all the data.
+    let data = 
+        '\r Task Name: ' + name.value + ' \r\n ' + 
+        'Task Description: ' + description.value + ' \r\n ' + 
+        'Assigned Person: ' + person.value + ' \r\n ' + 
+        'Due: ' + datepicker.value;
+    
+    // Convert the text to BLOB.
+    const textToBLOB = new Blob([data], { type: 'text/plain' });
+    const sFileName = 'formData.txt';	   // The file to save the data.
+
+    let newLink = document.createElement("a");
+    newLink.download = sFileName;
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    }
+    else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click(); 
+}
+
+*/
+
+//Found this Youtube video helpful: https://www.youtube.com/watch?v=Pc2e2YpKArg
+//And this followup video for multiple fields: https://www.youtube.com/watch?v=xvXtb7mwMd8 
+/*
+function formValidation() {
+    let errorMessage = "";
+
+    if (taskNameValue === "") {
+        errorMessage += "Name must be filled out. \n";
+        /*document.getElementById('formError').innerHTML = 'Name not filled out. You silly goose.'; */
+/*        }
+/*
+    if (taskDescriptionValue === "") {
+        errorMessage += "Description must be filled out. \n";
+        /*document.getElementById('formError').innerHTML = 'The Description was not filled out. You silly goose.'; */
+ /*   }
+
+    if (assignToValue === "") {
+        errorMessage += "Please assign someone this task. \n";
+        /*document.getElementById('formError').innerHTML = 'Please assign this task to someone. You silly goose.'; */
+/*    }
+
+    /*if (datePickerValue === "") {
+        errorMessage +=("Due Date must be filled out. \n");
+        document.getElementById('formError').innerHTML = 'Tells us when you would like this done by. You silly goose.';
+    } */
+
+ /*   if (errorMessage !== "") {
+        //alert(errorMessage);
+        document.getElementById('formError').innerHTML = errorMessage + "You silly goose.";
+        return false;
+    }
+    /*else {
+        console.log("Name:  " + taskNameValue);
+        console.log("Description:  " + taskDescriptionValue);
+        console.log("Assigned To:  " + assignToValue);
+        console.log("Due By:  " + datePickerValue);
+    }*/
+/*}
+*/
 
 /* This was the original attempt to create the function. 
 validFormFieldInput = (data) =>{
@@ -156,50 +227,4 @@ validFormFieldInput = (data) =>{
       }
     }
 }
-*/
-
-/*
-//Tanya's solution to validation challenge:
-
-let saveFile = () => {
-    	
-    // Get the data from each element on the form.
-    /* Tanya's variables for the element IDs. She used different ids than Michael in HTML
-    const name = document.getElementById('name');
-    const description = document.getElementById('description');
-    const person = document.getElementById('person');
-    const datepicker = document.getElementById('datepicker');
-    */
-/*   
-    // This variable stores all the data.
-    let data = 
-        '\r Task Name: ' + taskNameValue + ' \r\n ' + 
-        'Task Description: ' + taskDescriptionValue + ' \r\n ' + 
-        'Assigned Person: ' + assignToValue + ' \r\n ' + 
-        'Due: ' + datePickerValue;
-    
-    // Convert the text to BLOB.
-    const textToBLOB = new Blob([data], { type: 'text/plain' });
-    const sFileName = 'formData.txt';	   // The file to save the data.
-
-    let newLink = document.createElement("a");
-    newLink.download = sFileName;
-
-    if (window.webkitURL != null) {
-        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-    }
-    else {
-        newLink.href = window.URL.createObjectURL(textToBLOB);
-        newLink.style.display = "none";
-        document.body.appendChild(newLink);
-    }
-
-    newLink.click(); 
-}
-*/
-
-/*
-const test = new TaskManager();
-test.addTask('Michael', 'is awasome', 'Jennifer', '2/2/2022');
-console.log(test.task);
 */
--- resources/js/taskManager.js
+++ resources/js/taskManager.js
@@ -1,13 +1,15 @@
 class TaskManager {
     constructor() {
         this.task = ['item 1', 'item 2'];
-        this.currentId = currentId;
-        currentId = 0;
+        //currentId = 0;
+        //this.currentId = currentId;
     }
-   addTask(name1, description, assign, datePicker, status) {
+   /*addTask(name1, description, assign, datePicker, status) {
         this.currentId ++ ;
         this.task.push(this.currentId, name1, description, assign, datePicker, status = 'TODO')
-    }
+    } */
 } 
  
 //console.log(TaskManager);   
+
+
